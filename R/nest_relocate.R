4429b234ca8382d463d69757931454dcc2209303
Mark Rieke <markjrieke@gmail.com>
2022-06-13 20:09:26 -0500

    fix tidyeval of nest_relocate

--- R/nest_relocate.R
+++ R/nest_relocate.R
@@ -17,7 +17,7 @@ nest_relocate <- function(.data,
   # map relocate over list-col
   dplyr::mutate(
     .data,
-    "{{.nest_data}}" := purrr::map({{ .nest_data }}, ~dplyr::relocate(.x, !!!dots, .before = .before, .after = .after))
+    "{{.nest_data}}" := purrr::map({{ .nest_data }}, ~dplyr::relocate(.x, !!!dots, .before = {{ .before }}, .after = {{ .after }}))
   )
   
 }
