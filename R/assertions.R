b5c66954a77c5af7d3a62cbe5759516ab609023e
Mark Rieke <markjrieke@gmail.com>
2022-06-20 15:05:06 -0500

    add grouped_df class check

--- R/assertions.R
+++ R/assertions.R
@@ -24,8 +24,9 @@ assert_nest_df <- function(x) {
   )
   
   assertthat::assert_that(
-    assertthat::are_equal(class(x[1][[1]]), c("tbl_df", "tbl", "data.frame")),
-    msg = "argument `.nest_data` msut be of class \"tbl_df\", \"tbl\", \"data.frame\"."
+    assertthat::are_equal(class(x[1][[1]]), c("tbl_df", "tbl", "data.frame")) ||
+      assertthat::are_equal(class(x[1][[1]]), c("grouped_df", "tbl_df", "tbl", "data.frame")),
+    msg = "argument `.nest_data` msut be of class \"grouped_df\", \"tbl_df\", \"tbl\", \"data.frame\"."
   )
   
 }
