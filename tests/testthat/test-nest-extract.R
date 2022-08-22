f3152e496a52183df64f881d3a560e2495626ea6
joran <joran.elias@gmail.com>
2022-08-22 15:45:56 -0600

    test for nest_extract was wrong

--- tests/testthat/test-nest-extract.R
+++ tests/testthat/test-nest-extract.R
@@ -3,8 +3,8 @@ source("nplyr-reusable.R")
 
 test_that("nest_extract() returns tibble in expected format", {
   
-  set.seed(123)
-  gm_ext <- gm %>% mutate(comb = sample(c(NA, "a-b", "a-d", "b-c", "d-e"),size = nrow(gm),replace = TRUE))
+  gm <- gm_nest %>% tidyr::unnest(cols = c(country_data))
+  gm_ext <- gm %>% mutate(comb = rep(c(NA, "a-b", "a-d", "b-c", "d-e"),length.out = nrow(gm)))
   gm_nest_ext <- gm_ext %>% tidyr::nest(country_data = -continent)
   
   test_format(
@@ -20,14 +20,14 @@ test_that("nest_extract() returns tibble in expected format", {
 
 test_that("nest_extract() creates correct new columns",{
   
-  set.seed(123)
-  gm_ext <- gm %>% mutate(comb = sample(c(NA, "a-b", "a-d", "b-c", "d-e"),size = nrow(gm),replace = TRUE))
+  gm <- gm_nest %>% tidyr::unnest(cols = c(country_data))
+  gm_ext <- gm %>% mutate(comb = rep(c(NA, "a-b", "a-d", "b-c", "d-e"),length.out = nrow(gm)))
   gm_nest_ext <- gm_ext %>% tidyr::nest(country_data = -continent)
   
   x <- gm_nest_ext %>% nest_extract(.nest_data = country_data,col = comb,into = c("var1","var2"),regex = "([[:alnum:]]+)-([[:alnum:]]+)")
   
   expect_equal(x$country_data[[5]]$var1,
-               c("d", "d", "a", NA, "b", NA, NA, "a", "b", NA, "a", "d", "a", NA, "d", "a", NA, "a", "a", "a", "d", "d", "b", "a"))
+               rep(c("a", "b", "d", NA, "a"),length.out = 139))
   expect_equal(x$country_data[[5]]$var2,
-               c("e", "e", "d", NA, "c", NA, NA, "d", "c", NA, "d", "e", "b", NA, "e", "d", NA, "b", "b", "b", "e", "e", "c", "d"))
+               rep(c("d", "c", "e", NA, "b"),length.out = 139))
 })
